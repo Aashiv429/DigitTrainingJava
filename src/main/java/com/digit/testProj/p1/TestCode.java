fe3e530c85146030df4d33dec1ead555dcc2c33a
Aashiv429 <139847857+Aashiv429@users.noreply.github.com>
2023-07-18 12:41:42 +0530

    Update TestCode.java
    update change

--- src/main/java/com/digit/testProj/p1/TestCode.java
+++ src/main/java/com/digit/testProj/p1/TestCode.java
@@ -3,6 +3,7 @@ package com.digit.testProj.p1;
 public class TestCode {
 public static void main(String[] args) {
 	System.out.println("Just for testing");
+        System.out.println("Just for testing");
 	
 }
 }
