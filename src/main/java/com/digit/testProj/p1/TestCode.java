44a3506ff71628fce2ec790ae99ed5451fa70136
Ashi.Verma <Ashi.Verma@LDIGITBGL-L241.godigit.corp>
2023-07-18 12:31:27 +0530

    changed

--- src/main/java/com/digit/testProj/p1/TestCode.java
+++ src/main/java/com/digit/testProj/p1/TestCode.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
 	System.out.println("hii");
 	System.out.println("hii");
 	System.out.println("hii");
-	System.out.println("hii");
+	System.out.println("I am ashi");
+	
 }
 }
