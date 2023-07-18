bd837b52d3ca6b9b4a6e01889f8474a77d53e798
Ashi.Verma <Ashi.Verma@LDIGITBGL-L241.godigit.corp>
2023-07-18 12:32:54 +0530

    first commit

--- src/main/java/com/digit/testProj/p1/TestCode.java
+++ src/main/java/com/digit/testProj/p1/TestCode.java
@@ -2,10 +2,7 @@ package com.digit.testProj.p1;
 
 public class TestCode {
 public static void main(String[] args) {
-	System.out.println("hii");
-	System.out.println("hii");
-	System.out.println("hii");
-	System.out.println("I am ashi");
+	System.out.println("Just for testing");
 	
 }
 }
