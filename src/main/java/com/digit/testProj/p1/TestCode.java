599f8c9c78d3321e59ee5a23e6b79074331f94f3
Ashi.Verma <Ashi.Verma@LDIGITBGL-L241.godigit.corp>
2023-07-18 12:25:39 +0530

    This is my first commit

---
+++ src/main/java/com/digit/testProj/p1/TestCode.java
@@ -0,0 +1,10 @@
+package com.digit.testProj.p1;
+
+public class TestCode {
+public static void main(String[] args) {
+	System.out.println("hii");
+	System.out.println("hii");
+	System.out.println("hii");
+	System.out.println("hii");
+}
+}
