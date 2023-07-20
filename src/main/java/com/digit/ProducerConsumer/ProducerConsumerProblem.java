4d9b07e941307040ff59b6476e13f1347193173e
Ashi.Verma <Ashi.Verma@LDIGITBGL-L241.godigit.corp>
2023-07-20 11:06:35 +0530

    .

--- src/main/java/com/digit/ProducerConsumer/ProducerConsumerProblem.java
+++ src/main/java/com/digit/ProducerConsumer/ProducerConsumerProblem.java
@@ -1,11 +1,12 @@
 package com.digit.ProducerConsumer;
+
 public class ProducerConsumerProblem {
-public static void main(String[] args) {
-	Queue1 q =new Queue1();
-	Producer p = new Producer(q);
-	Consumer c = new Consumer(q);
-	
-	p.start();
-	c.start();
-}
+	public static void main(String[] args) {
+		Queue1 q = new Queue1();
+		Producer p = new Producer(q);
+		Consumer c = new Consumer(q);
+
+		p.start();
+		c.start();
+	}
 }
